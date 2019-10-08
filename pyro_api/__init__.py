6397d3d8897b7a0c8795d46287b054edf91b4389
Neeraj Pradhan <npradhan@uber.com>
2019-10-08 12:53:57 -0700

    Seed repo with pyro.generic

---
+++ pyro_api/__init__.py
@@ -0,0 +1,11 @@
+from pyro_api.dispatch import distributions, handlers, infer, ops, optim, pyro, pyro_backend
+
+__all__ = [
+    'distributions',
+    'handlers',
+    'infer',
+    'ops',
+    'optim',
+    'pyro',
+    'pyro_backend',
+]
